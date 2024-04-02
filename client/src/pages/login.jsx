80d5017fa01d6b6feff6d02e79ba40f92bd13522
kmb8043 <you@example.com>
2024-04-02 19:52:08 -0400

    fixed the bugs and added subscribe

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,30 +1,30 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 
-
 const Login = () => {
-    return(
+    return (
         <div>
             <div className="">
                 <h1> Login </h1>
                 <form action="">
                     <div>
-                        <input type="email"/>
-                        <label htmlFor=""> Email </label>
+                        <input type="email" placeholder="Email"/>
+                        <label htmlFor=""> </label>
                         <MdEmail size={20} />
                     </div>
                     <div>
-                        <input type="password"/>
-                        <label htmlFor=""> Password </label>
+                        <input type="password" placeholder="Password"/>
+                        <label htmlFor=""></label>
                         <RiLockPasswordLine size={20} />
                     </div>
                     <button type="submit"> Login </button>
-                    <span> Sign Up <Link to='Signup'> Click Here to Join</Link> </span>
+                    <span> Sign Up <Link to='/signup'> Click Here to Join</Link> </span>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Login;
--- client/src/pages/subscription.jsx
+++ client/src/pages/subscription.jsx
@@ -2,13 +2,13 @@ import React from "react"
 
 const Subscription = () => {
     return(
-        <div className="container">
+        <div >
             <div>
-            <div>
-                <h2>Basic</h2>
+            <div className="container"> 
+                <h2>Basic 30 </h2>
                 <p>Gives you 30 downloads a month</p>
                 <p>Lower quality photos</p>
-                <p>$29</p> <p>/month</p>
+                <p>$29/month</p>
             </div>
             <button> Subscribe </button>
             </div>
@@ -19,7 +19,7 @@ const Subscription = () => {
                 <h2>Premium</h2>
                 <p>Gives you up to 100 downloads a month</p>
                 <p>High quality phots</p>
-                <p>$70</p> <p>/month</p>
+                <p>$70/month</p>
             </div>
             <button> Subscribe </button>
             </div>
@@ -28,4 +28,4 @@ const Subscription = () => {
     ) 
 }
 
-export default Subscription
+export default Subscription;
