2d405c278cbbbca89a37aa12be1a7eb180f977d0
Tina Humphrey <humphrey14t@gmail.com>
2024-04-02 20:13:56 -0400

    commit modiefied changes in main.jsx and login.jsx to test out page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
+import ReactDOM from 'react-dom/client';
 import Subscription from "./pages/subscription";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 
-const React = createBrowserRouter([
+const router = createBrowserRouter([
     {
-        path:'/login',
+        path:'/',
         element:<Login />
     },
     {
@@ -18,9 +19,9 @@ const React = createBrowserRouter([
     }
 ])
 
-ReactDOM.createRoot(document.getElmentById('root')).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // <React.StrictMode>
         <RouterProvider router={router} />
-    </React.StrictMode>
+    // </React.StrictMode>
 )
 
