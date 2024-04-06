923591822765e6dd6f7b785b19d1769c5aca34f4
Gordon <gordonjackman@gmail.com>
2024-04-06 19:28:40 -0400

    updated the resolvers fixing a syntax issue.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,8 +21,8 @@ const resolvers = {
         }
     },
     Mutation: {
-        addUser: async (parent, { firstName, lastName,  email, password }) => {
-            const user = await User.create({ firstName, lastName, email, password });
+        addUser: async (parent, { username,  email, password }) => {
+            const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
         },
